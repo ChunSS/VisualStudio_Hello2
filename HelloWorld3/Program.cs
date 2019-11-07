09f0a9506e04e298d9c60038df9571448f8905fc
KangSukChun <ksc9910@naver.com>
2019-11-07 09:47:46 +0900

    hotfix WriteLine Commit

--- HelloWorld3/Program.cs
+++ HelloWorld3/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("hotfix : Hello World");
         }
     }
 }
